3437731028d0cdbdddc31e4841c7936f3bfbf04c
JensHoRi <47488279+JensHoRi@users.noreply.github.com>
2023-01-08 02:01:13 +0100

    Update openhab_connector.cpp

--- src/openhab_connector.cpp
+++ src/openhab_connector.cpp
@@ -317,7 +317,7 @@ int Sitemap::openlink(const char* url)
             {
                 if (widget["linkedPage"]["link"])
                     item->setType(ItemType::type_link);
-                else if (widget["item"]["type"] && widget["item"]["type"].as<String>() == "Number")
+                else if (widget["item"]["type"] && widget["item"]["type"].as<String>() >= "Number" && widget["item"]["type"].as<String>() <= "Number:Z")
                     item->setType(ItemType::type_number);
                 else
                     item->setType(ItemType::type_string);
